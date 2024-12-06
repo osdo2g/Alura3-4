3c3392c08a2238aaf35c439c7c6609ea495a2f08
osdo2g <00001091156773sp@al.educacao.sp.gov.br>
2024-12-06 15:43:08 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Artes',
+    'Quem pintou a Monalisa?',
+    'Foi Leonardo de Vinci'
+)
+
+criaCartao(
+    'Geografia',
+    'Em qual continente fica a Austrália?',
+    'Fica na Oceania'
+)
+
+criaCartao(
+    'Conhecimentos Gerais',
+    'Quando é celebrado o dia da independencia no Brasil?',
+    'No dia 7 de Setembro'
+)
+
+criaCartao(
+    'História',
+    'Quem descobriu a América?',
+    'Foi Cristóvão Colombo'
+)
